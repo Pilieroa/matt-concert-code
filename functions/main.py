f929d7c4d869fef218035666be68a13397136c95
Andrew Piliero <afpiliero@gmail.com>
2025-03-24 17:42:20 -0700

    - cleaned up imports, improved gitignore

--- functions/main.py
+++ functions/main.py
@@ -1,10 +1,7 @@
 import firebase_admin
 from firebase_functions import scheduler_fn
 
-from bs4 import BeautifulSoup
-from dataclasses import dataclass
-from datetime import datetime
-import requests
+import os
 import resend
 
 from html_service import get_html_from_events
@@ -13,8 +10,8 @@ from scraping_service import get_upcoming_events
 firebase_admin.initialize_app()
 
 
-@scheduler_fn.on_schedule(schedule="0 9 * * 0")
-def send_concerts():
+@scheduler_fn.on_schedule(schedule="1 9 * * 0")
+def send_concerts(event):
     resend.api_key = os.environ.get("RESEND_API_KEY")
     html = get_html_from_events(get_upcoming_events())
     resend.Emails.send(
